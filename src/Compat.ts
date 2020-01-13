310405b72a476bce09aaf91eee11a6492d7ced9c
daybrush <daybrush@gmail.com>
2020-01-14 05:53:55 +0900

    feat: add provider

--- src/Compat.ts
+++ src/Compat.ts
@@ -1,10 +1,43 @@
-
 import { diff } from "@egjs/list-differ";
+import { IObject } from "@daybrush/utils";
 
-function diffObject(a: object, b: object) {
+function isDiff(a: object, b: object) {
+    if (a === b) return false;
 	for (let i in a) if (!(i in b)) return true;
 	for (let i in b) if (a[i] !== b[i]) return true;
 	return false;
+}
+function diffObject(a: object, b: object) {
+    const keys1 = Object.keys(a);
+    const keys2 = Object.keys(b);
+    const result = diff(keys2, keys2, key => key);
+
+    const added: IObject<any> = {};
+    const removed: IObject<any> = {};
+    const changed: IObject<any> = {};
+
+    result.added.forEach(index => {
+        const name = keys2[index];
+
+        added[name] = b[index];
+    });
+    result.removed.forEach(index => {
+        const name = keys1[index];
+
+        removed[name] = a[index];
+    });
+    result.maintained.forEach(([index]) => {
+        const name = keys1[index];
+
+        if (a[name] !== b[name]) {
+            changed[name] = [a[name], b[name]];
+        }
+    });
+    return {
+        added,
+        removed,
+        changed,
+    }
 }
 function fillKeys(keys: any[]): string[] {
     let index = 0;
@@ -30,20 +63,109 @@ export function createElement(
         props: {...otherProps, children },
     };
 }
-export class Provider {
+export abstract class Provider<T extends Element | Component | Node = Element | Component | Node> {
+    type: any;
     key: string;
-    base: Element | Component;
+    ref: (e: Element | Component | Node) => any;
+    base: T;
+    props = {};
+    _providers: Provider<any>[] = [];
+    abstract _should(nextProps: any, nextState: any): boolean;
+    abstract _render(prevProps: any, nextState: any, hooks: Function[]);
+    public _update(nextProps: any, nextState: any, hooks: Function[]) {
+        // shouldComponentUpdate
+        if (!this._should(nextProps, nextState)) {
+            return;
+        }
+        // render
+        const prevProps = this.props;
+        this.props = nextProps;
+        this._render(prevProps, nextState, hooks);
+    }
+    public _mounted() {
+        this.ref(this.base);
+    }
+    public _updated() {
+        this.ref(this.base);
+    }
+    public _destroy() {
+        this.ref(null);
+    }
+}
+export class ElementProvider extends Provider<Element> {
+    public _should(nextProps: any) {
+        return isDiff(this.props, nextProps);
+    }
+    public _render(prevProps, _, hooks: Function[]) {
+        const isMount = !this.base;
+
+        if (isMount) {
+            this.base = document.createElement(this.type);
+        }
+        const base = this.base;
+        const { added, changed, removed } = diffObject(prevProps, this.props);
+
+        for (const name in added) {
+            base.setAttribute(name, added[name]);
+        }
+        for (const name in changed) {
+            base.setAttribute(name, changed[name][1]);
+        }
+        for (const name in removed) {
+            base.removeAttribute(name);
+        }
+        hooks.push(() => {
+            if (isMount) {
+                this._mounted();
+            } else {
+                this._updated();
+            }
+        });
+    }
+}
+export class ComponentProvider extends Provider<Component> {
+    public _should(nextProps: any, nextState: any) {
+        return this.base.shouldComponentUpdate(nextProps, nextState);
+    }
+    public _render(prevProps, nextState, hooks: Function[]) {
+        const isMount = !this.base;
+
+        if (isMount) {
+            this.base = new this.type(this.props);
+        }
+        const base = this.base;
+        const prevState = base.state;
+        base.state = nextState;
+        const template = base.render();
+
+        hooks.push(() => {
+            if (isMount) {
+                this._mounted();
+                base.componentDidMount();
+            } else {
+                this._updated();
+                base.componentDidUpdate(prevProps, prevState);
+            }
+        });
+    }
 }
 export class Component {
+    _provider: Provider;
     props = {};
     state = {};
-    public base!: Element | null;
-    private _components = [];
-    
+    public shouldComponentUpdate(props?: any, state?: any): boolean {
+        return true;
+    }
+    public render() {
+        return null;
+    }
+    public componentDidMount() {}
+    public componentDidUpdate(prevProps, prevState) {}
+
 }
 export class PureComponent extends Component {
-    shouldComponentUpdate(props, state) {
-        return diffObject(this.props, props) || diffObject(this.state, state);
+    public shouldComponentUpdate(props?, state?) {
+        return isDiff(this.props, props) || isDiff(this.state, state);
     }
 }
 
